3729cf7c0352cbb3ea3cfedec5af7151531adb33
DarkBorder <45429524+DarkBorder@users.noreply.github.com>
2021-12-24 05:17:38 +0300

    Sepet Guncellendi

---
+++ Models/Sepet.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeBox.Models
+{
+    public class Sepet
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public int? AnimeId { get; set; }
+        [ForeignKey("AnimeId")]
+        public Anime Anime { get; set; }
+        public DateTime? SepeteEklenmeTarihi { get; set; }
+
+        public bool? SiparisOk { get; set; } = false;
+
+      
+      
+    }
+}
